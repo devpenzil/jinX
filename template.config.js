c5a6422e8b5049e4f2a7dc0052dcc5f88282d65a
Ajo Alex <67946056+devpenzil@users.noreply.github.com>
2024-09-11 17:49:23 +0530

    new boilerplate

--- template.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    placeholderName: 'Boilerplate',
-    titlePlaceholder: 'Devpenzil React Native Boilerplate',
-    templateDir: './template',
-    // postInitScript: './post-init.script.js',
-};
