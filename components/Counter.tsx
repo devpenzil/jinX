f44d549eb0871f0e152a3815dfce8654cd4d0277
Ajo Alex <67946056+devpenzil@users.noreply.github.com>
2024-11-21 00:28:27 +0530

    setting up

---
+++ components/Counter.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Counter = () => {
+  return (
+    <View>
+      <Text>Counter</Text>
+    </View>
+  );
+};
+
+export default Counter;
